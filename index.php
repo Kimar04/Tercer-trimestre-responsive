1e063662c72e66c2943a1f529226b71d5b8246d6
Kimar04 <kimar041604@gmail.com>
2018-07-23 23:43:18 -0500

    Tercer trimestre responsive

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>responsive</title>
+</head>
+<body>
+<header>
+	<h1>Responsive</h1>
+</header>
+<section>
+	<article>
+		<p>Parrafo</p>
+	</article>
+	<article>
+	</article>
+	<article>
+	</article>
+	<article>
+	</article>
+</section>
+<aside>
+</aside>
+<footer>
+	<div class="containerbox">
+	</div>
+	<div class="containerbox">
+	</div>
+	<div class="containerbox">
+	</div>
+	<div class="containerbox">
+	</div>
+	</footer>
+</body>
+</html>
